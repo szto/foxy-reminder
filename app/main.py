6f9edca322766f8c80a89047cd795ead9cddfe5a
szto <sztolabs@gmail.com>
2024-03-17 17:37:08 +0900

    add dummy template

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ This module is the main module for the FastAPI app.
 # --------------------------------------------------------------------------------
 
 from fastapi import FastAPI
-from app.routers import api, login
+from app.routers import api, login, pages
 
 
 # --------------------------------------------------------------------------------
@@ -17,6 +17,7 @@ from app.routers import api, login
 app = FastAPI()
 app.include_router(api.router)
 app.include_router(login.router)
+app.include_router(pages.router)
 
 
 # --------------------------------------------------------------------------------
