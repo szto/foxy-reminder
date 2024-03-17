96eef53d8f6b425b55764ba1ff1147a1550fa1a4
szto <sztolabs@gmail.com>
2024-03-17 16:57:34 +0900

    refoctor: add auth router

--- app/routers/api.py
+++ app/routers/api.py
@@ -138,21 +138,3 @@ async def delete_lists_id(list_id: int) -> dict:
   """
 
   return dict()
-
-
-class UserAccount(BaseModel):
-  username: str
-  password: str
-
-
-@router.post("/login", summary="Logs into the app")
-async def post_login(user: UserAccount, response: Response) -> dict():
-  response.set_cookie(key="session", value=user.username)
-  return {"message": f"Logged in as {user.username}"}
-
-
-
-@router.post("/logout", summary="Logs out of the app")
-async def post_login(response: Response, session: str | None = Cookie(default=None)) -> dict():
-  response.set_cookie(key="session", value=session, expires=-1)
-  return {"message": f"Logged out as {session}"}
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ This module is the main module for the FastAPI app.
 # --------------------------------------------------------------------------------
 
 from fastapi import FastAPI
-from app.routers import api
+from app.routers import api, auth
 
 
 # --------------------------------------------------------------------------------
@@ -16,6 +16,7 @@ from app.routers import api
 
 app = FastAPI()
 app.include_router(api.router)
+app.include_router(auth.router)
 
 
 # --------------------------------------------------------------------------------
