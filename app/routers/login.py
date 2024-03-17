5ba38c2919edc2ac8cfc141da04801c8ff268613
szto <sztolabs@gmail.com>
2024-03-17 17:45:34 +0900

    Login form

--- app/routers/login.py
+++ app/routers/login.py
@@ -6,7 +6,7 @@ This module provides routes for authentication.
 # Imports
 # --------------------------------------------------------------------------------
 
-from ..auth import *
+from ..auth import AuthCookie, get_login_form_creds, get_auth_cookie
 
 from fastapi import APIRouter, Cookie, Depends, Response
 from pydantic import BaseModel
@@ -33,13 +33,12 @@ class UserAccount(BaseModel):
 # --------------------------------------------------------------------------------
 
 @router.post("/login", summary="Logs into the app")
-async def post_login(response: Response, user_token: UserToken = Depends(get_http_basic_token)) -> dict:
-    print(user_token)
-    response.set_cookie(key=auth_cookie, value=user_token.token)
-    return {"message": f"Logged in as {user_token.username}"}
+async def post_login(response: Response, cookie: AuthCookie = Depends(get_login_form_creds)) -> dict:
+    response.set_cookie(key=cookie.name, value=cookie.token)
+    return {"message": f"Logged in as {cookie.username}"}
 
 
 @router.post("/logout", summary="Logs out of the app")
-async def post_login(response: Response, user_token: UserToken = Depends(get_auth_cookie_token)) -> dict:
-    response.set_cookie(key=auth_cookie, value=user_token.token, expires=-1)
-    return {"message": f"Logged out as {user_token.username}"}
+async def post_login(response: Response, cookie: AuthCookie = Depends(get_auth_cookie)) -> dict:
+    response.set_cookie(key=cookie.name, value=cookie.token, expires=-1)
+    return {"message": f"Logged out as {cookie.username}"}
