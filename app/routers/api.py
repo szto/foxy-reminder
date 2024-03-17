e76045b694e42f3878accde36e27ead1dcde92f2
szto <sztolabs@gmail.com>
2024-03-17 16:53:31 +0900

    feat: apply to async

--- app/routers/api.py
+++ app/routers/api.py
@@ -8,7 +8,7 @@ This module provides routes for status.
 
 from .. import db
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Cookie, Response
 from pydantic import BaseModel
 
 
@@ -49,7 +49,7 @@ class UpdatedReminderList(BaseModel):
 # --------------------------------------------------------------------------------
 
 @router.get("/lists", summary="Get the user's reminder lists", response_model=list[ReminderList])
-def get_lists():
+async def get_lists() -> list[ReminderList]:
   """
   Gets the list of all reminder lists owned by the user.
   """
@@ -80,7 +80,7 @@ def get_lists():
 
 
 @router.post("/lists", summary="Create a new reminder list", response_model=ReminderList)
-def post_lists(reminder_list: NewReminderList):
+async def post_lists(reminder_list: NewReminderList) -> ReminderList:
   """
   Creates a new reminder list for the user.
   """
@@ -98,7 +98,7 @@ def post_lists(reminder_list: NewReminderList):
 
 
 @router.get("/lists/{list_id}", summary="Get a reminder list by ID", response_model=ReminderList)
-def get_lists_id(list_id: int):
+async def get_lists_id(list_id: int) -> ReminderList:
   """
   Gets the reminder list by ID.
   """
@@ -115,7 +115,7 @@ def get_lists_id(list_id: int):
   )
 
 @router.put("/lists/{list_id}", summary="Fully updates a reminder list", response_model=ReminderList)
-def put_lists_id(reminder_list: UpdatedReminderList):
+async def put_lists_id(reminder_list: UpdatedReminderList) -> ReminderList:
   """
   Fully updates a reminder list for the user.
   """
@@ -132,7 +132,7 @@ def put_lists_id(reminder_list: UpdatedReminderList):
   )
 
 @router.delete("/lists/{list_id}", summary="Deletes a reminder list", response_model=dict)
-def delete_lists_id(list_id: int):
+async def delete_lists_id(list_id: int) -> dict:
   """
   Deletes the reminder list by ID.
   """
