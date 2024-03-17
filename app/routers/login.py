fb9f6870d6b144d35b7d486e9ad77cc4d7fe390b
szto <sztolabs@gmail.com>
2024-03-17 18:01:13 +0900

    feat: Added reminders pagee stub with login redirect

--- app/routers/login.py
+++ app/routers/login.py
@@ -6,11 +6,11 @@ This module provides routes for authentication.
 # Imports
 # --------------------------------------------------------------------------------
 
+from app import templates
 from app.utils.auth import AuthCookie, get_login_form_creds, get_auth_cookie
 
 from fastapi import APIRouter, Depends, Request, Response
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, RedirectResponse
 from pydantic import BaseModel
 
 
@@ -42,7 +42,7 @@ async def get_login(request: Request):
 @router.post("/login", summary="Logs into the app")
 async def post_login(response: Response, cookie: AuthCookie = Depends(get_login_form_creds)) -> dict:
   response.set_cookie(key=cookie.name, value=cookie.token)
-  return {"message": f"Logged in as {cookie.username}"}
+  return response
 
 @router.post("/logout", summary="Logs out of the app")
 async def post_login(response: Response, cookie: AuthCookie = Depends(get_auth_cookie)) -> dict:
