5ebc92c71e05849f65570db4dccf2a55f30a6a65
szto <sztolabs@gmail.com>
2024-03-19 04:56:00 +0900

    feat: separate main and root

--- app/main.py
+++ app/main.py
@@ -7,19 +7,18 @@ This module is the main module for the FastAPI app.
 # --------------------------------------------------------------------------------
 
 from fastapi import FastAPI, Request
-from fastapi.responses import FileResponse, RedirectResponse
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
-from app.routers import api, login, reminders
+from app.routers import api, login, reminders, root
 from app.utils.exceptions import UnauthorizedPageException
-from app import templates
-
 
 # --------------------------------------------------------------------------------
 # App Creation
 # --------------------------------------------------------------------------------
 
 app = FastAPI()
-# app.include_router(api.router)
+app.include_router(root.router)
+app.include_router(api.router)
 app.include_router(login.router)
 app.include_router(reminders.router)
 
@@ -39,23 +38,3 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.exception_handler(UnauthorizedPageException)
 async def unauthorized_exception_handler(request: Request, exc: UnauthorizedPageException):
     return RedirectResponse("/login?unauthorized=True", status_code=302)
-
-
-# --------------------------------------------------------------------------------
-# Routes
-# --------------------------------------------------------------------------------
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/favicon.ico", include_in_schema=False)
-async def favicon():
-    return FileResponse("static/img/favicon.ico")
-
-
-@app.get("/not-found")
-async def get_not_found(request: Request):
-    return templates.TemplateResponse("pages/not-found.html", {"request": request})
---
+++ app/routers/root.py
@@ -0,0 +1,47 @@
+from typing import Optional
+
+from starlette.responses import FileResponse, RedirectResponse
+
+from fastapi import APIRouter, Depends, Request
+from app import templates
+from app.utils.auth import AuthCookie, get_auth_cookie
+
+# --------------------------------------------------------------------------------
+# Router
+# --------------------------------------------------------------------------------
+
+router = APIRouter()
+
+
+# --------------------------------------------------------------------------------
+# Routes
+# --------------------------------------------------------------------------------
+
+
+@router.get(
+    path="/",
+    summary="Redirect to the login or reminders pages",
+    tags=["Pages"],
+)
+def read_root(
+    cookie: Optional[AuthCookie] = Depends(get_auth_cookie),
+):
+    path = "/reminders" if cookie else "/login"
+    return RedirectResponse(url=path, status_code=302)
+
+
+@router.get(
+    path="/favicon.ico",
+    include_in_schema=False,
+)
+async def favicon():
+    return FileResponse("static/img/favicon.ico")
+
+
+@router.get(
+    path="/not-found",
+    summary="Gets the \"Not Found\" page",
+    tags=["Pages"],
+)
+async def get_not_found(request: Request):
+    return templates.TemplateResponse("pages/not-found.html", {"request": request})
